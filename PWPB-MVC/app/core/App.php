70e66313d3c3e76facbea5312a0b203a8142471b
prima's <69377855+broprims@users.noreply.github.com>
2021-10-12 23:01:48 +0800

    Add files via upload

--- PWPB-MVC/app/core/App.php
+++ PWPB-MVC/app/core/App.php
@@ -1,10 +1,23 @@
 <?php
 
+
+
 class App
 {
 
+    public function parseURL() {
+        if(isset($_GET["url"])) {
+            $url = rtrim($_GET["url"],'/');
+            $url = filter_var($url, FILTER_SANITIZE_URL);
+            $url = explode("/", $url);
+            return $url;
+        }
+    }
+
+
     public function __construct() {
-        echo "Web MVC Siap Dijalankan!";
+        $url = $this->parseURL();
+        var_dump($url);
     }
 
 
