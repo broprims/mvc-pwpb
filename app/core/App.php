bb961a28b3c814125df79f331e02feac9efd5af0
satriaprima <satria040503@gmail.com>
2021-10-21 22:21:05 +0800

    #4 Controller

--- app/core/App.php
+++ app/core/App.php
@@ -4,6 +4,9 @@
 
 class App
 {
+    protected $controller = 'Home';
+    protected $method = 'index';
+    protected $params = [];
 
     public function parseURL() {
         if(isset($_GET["url"])) {
@@ -17,8 +20,25 @@ class App
 
     public function __construct() {
         $url = $this->parseURL();
-        var_dump($url);
-    }
 
+        //setup controller
+        if(file_exists("../app/controllers/". $url[0].".php")) {
+            $this->controller = $url[0];
+            unset($url[0]);
+        }
+        require_once "../app/controllers/". $this->controller.".php";
+        $this->controller = new $this->controller;
+
+        if(isset($url[1])) {
+            if(method_exists($this->controller, $url[1])) {
+                $this->method= $url[1];unset($url[1]);
+            }
+        }
+
+        if(!empty($url)) {
+            $this->params = array_values($url);
+        }
+        call_user_func_array([$this->controller, $this->method], $this->params);
+    }
 
 }
