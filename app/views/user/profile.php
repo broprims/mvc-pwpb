d7471cd13249f51800fb03c4f29ddc5966d36cc7
satriaprima <satria040503@gmail.com>
2021-11-03 12:16:26 +0800

    #6 Asset

--- app/views/user/profile.php
+++ app/views/user/profile.php
@@ -1,3 +1,5 @@
 
-<h1>Halaman User</h1>
+<div class="container text-center mt-4">
+<h1>Halaman User</h1><img src="<?= BASE_URL; ?>/img/s.jpeg" class="rounded-circle shadow border-4 w-25">
 <p>Halo, nama saya <?= $data['nama']; ?>, saya seorang <?= $data['pekerjaan'];?></p>
+</div>
